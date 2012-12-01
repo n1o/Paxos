5eeae08798749a4b941022042c815ef8d1d0da89
marek <marek@linuxmint.linuxmint-domain>
2012-12-01 21:01:33 +0100

    Changign acceptor, and proposer

--- Paxos/sk/marek/barak/paxos/Acceptor.py
+++ Paxos/sk/marek/barak/paxos/Acceptor.py
@@ -6,19 +6,63 @@ Created on Nov 30, 2012
 import SocketServer
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
-    
+    def __init__(self, request, client_address, server):
+        print "INIT"
+        self.PROPOSE = "PROPOSE"
+        self.ACCEPT = "ACCEPT"
+        self.PROMIS = "PROMIS"
+        self.lastAccepted = self.getLastAccepted()
+        SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
+        
+        
     def handle(self):
         # self.request is the TCP socket connected to the client
+        
         self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        self.data = "Server 1: " + self.data
+        elements = self.parseMessage(self.data)
+        lastAccepted = int(elements.pop(0))
+        if self.lastAccepted < lastAccepted and self.PROPOSE == elements.pop(0):
+            print "handle propose"
+            self.writeLastAccepted(lastAccepted)
+            self.lastAccepted = lastAccepted
+            self.data = str(self.lastAccepted)+";"+self.PROMIS
+            for item in elements:
+                self.data+=";"+item
+        elif self.lastAccepted < lastAccepted and self.ACCEPT == elements.pop(0):
+            print "handle accept"
+        else:
+            print "hanlde reject:"
+            
         print self.data
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
-
+    def parseMessage(self,data):
+        val = data.split(";")
+        elements = list()
+        for item in val:
+            if not item == "":
+                elements.append(item)
+        return elements
+    
+    def getLastAccepted(self):
+        try:
+            f = open("./conf","r+")
+            line = f.readline();
+            if line != "":
+                return int(line)
+            else: return 0
+        except IOError:
+            print "NOT FOUND"
+            
+    def writeLastAccepted(self,accepted):
+        try:
+            f = open("./conf","r+")
+            f.write(str(accepted))
+        except IOError:
+            print "NOT FOUND"
+            
 if __name__ == "__main__":
     HOST, PORT = "192.168.28.1", 9999
-
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
 
--- Paxos/sk/marek/barak/paxos/Proposer.py
+++ Paxos/sk/marek/barak/paxos/Proposer.py
@@ -1,4 +1,6 @@
 import socket
+from threading import Timer
+import time
 from Queue import Queue
 
 
@@ -12,9 +14,12 @@ class ProposerServer(object):
         self.messageQueue = Queue()
         self.PROPOSE = "PROPOSE"
         self.ACCEPT = "ACCEPT"
-        self.LEADER_BONUS = 3000000
-        self.isLeader = False
-        
+        self.LEADER_BONUS = 500000
+        self.LEADER = False
+        self.CREATE = "CREATE"
+        self.UPDATE = "UPDATE"
+        self.DROP = "DROP"
+        self.PROMIS = "PROMIS"
     def send(self,message,host):
         try:
             # Create a socket (SOCK_STREAM means a TCP socket)
@@ -27,32 +32,60 @@ class ProposerServer(object):
             return sock.recv(1024)
         finally:
             sock.close()
+            
     def sendAll(self,message):
+        responses = list()
         for host in self.HOSTS:
-            message = self.send(message, host)
-            print message
+            response = self.send(message, host)
+            responses.append(response)
+        
             
     def getValue(self,leader):
-        import time
         val = int(round(time.time() * 100000))
         if leader:
             val += self.LEADER_BONUS
         return val
         
-    def createMessage(self,messageType,messageList,leader):
+    def createMessage(self,messageType,messageList):
+        self.setLeader()
         message = ""
-        message +=str(self.getValue(leader))+ ";"
+        message +=str(self.getValue(self.LEADER))+ ";"
         message +=messageType + ";"
         for item in messageList:
             message+=item +";"
         return message
+    def isPromis(self,items):
+        for item in items:
+            if self.parseMessage(item).pop(1)!=self.PROMIS:
+                return False
+        return True
     
-        
+    def getInterval(self):
+        now = time.localtime()
+        val =  60 - now.tm_sec
+        return val
+    
+    def setLeader(self):
+        val = self.getInterval()/10
+        if val % 2 == 1:
+            self.LEADER = True
+            
+    def parseMessage(self,data):
+        val = data.split(";")
+        elements = list()
+        for item in val:
+            if not item == "":
+                elements.append(item)
+        return elements
 proposer = ProposerServer()
 l = list()
+l.append(proposer.CREATE)
 l.append("THing")
 l.append("JOP")
 
-data = proposer.createMessage(proposer.PROPOSE,l, False)
+data = proposer.createMessage(proposer.PROPOSE,l)
 proposer.sendAll(data)
+
+    
+    
 
